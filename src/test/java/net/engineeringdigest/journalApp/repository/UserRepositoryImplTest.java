ca359d8daf77e882d7e6b8edc4c5853fd4a99e17
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-02 23:58:56 +0530

    For checking for sonar

--- src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
+++ src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
@@ -1,17 +1,37 @@
 package net.engineeringdigest.journalApp.repository;
-
+import net.engineeringdigest.journalApp.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 public class UserRepositoryImplTest {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private UserRepositoryImpl userRepository;
 
+    //    @Test
+//    public void testingRepository(){
+//        userRepository.listUserForSA();
+//    }
     @Test
-    public void testingRepository(){
-        userRepository.listUserForSA();
+    public void listUserForSA_ShouldReturnValidUsers() {
+        // Insert test data
+        User validUser = new User();
+        validUser.setEmail("valid@example.com");
+        validUser.setSentimentAnalysis(true);
+        mongoTemplate.save(validUser);
+
+        // Test
+        List<User> result = userRepository.listUserForSA();
+        assertEquals(1, result.size());
     }
 }
+
+
