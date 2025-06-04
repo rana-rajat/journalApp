e7f32bb9238dbd81f9a826f7b8b5df6513c38ac8
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-05 00:40:57 +0530

    complete code for mail sender

--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
@@ -1,7 +1,17 @@
 package net.engineeringdigest.journalApp.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class EmailServiceTest {
+
+    @Autowired
+    EmailService emailService;
+
+    @Test
+    public void testSendMail(){
+        emailService.sendEmail("rajatrana2309@gmail.com","YheLe","Sending the mail for checking ");
+    }
 }
