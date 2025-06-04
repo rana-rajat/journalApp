02a189a9eaab011d0dacb3ef06289a14e2db2f1c
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-05 00:50:20 +0530

    trying to pass the test cases for sonar qube

--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
@@ -3,15 +3,19 @@ package net.engineeringdigest.journalApp.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 public class EmailServiceTest {
-
+    
+    @MockBean
+    private JavaMailSender javaMailSender;
     @Autowired
-    EmailService emailService;
+    private EmailService emailService;
 
     @Test
-    public void testSendMail(){
-        emailService.sendEmail("rajatrana2309@gmail.com","YheLe","Sending the mail for checking ");
+    public void testSendMail() {
+        emailService.sendEmail("rajatrana2309@gmail.com", "YheLe", "Sending the mail for checking ");
     }
 }
