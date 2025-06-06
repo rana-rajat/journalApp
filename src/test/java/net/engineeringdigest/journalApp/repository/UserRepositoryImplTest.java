3f763ff4ff886554e39c411c0213184c217aa04c
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-06 22:22:21 +0530

    testing

--- src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
+++ src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 public class UserRepositoryImplTest {
 //    @Autowired
 //    private UserRepositoryImpl userRepository;
