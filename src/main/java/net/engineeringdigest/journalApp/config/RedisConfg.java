9a4505ab355c12ad3a59a8b154e09bf93c421e46
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-09 23:32:11 +0530

    Redis config

---
+++ src/main/java/net/engineeringdigest/journalApp/config/RedisConfg.java
@@ -0,0 +1,21 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfg {
+
+    // why this clas basically when we are setting some value in redis cli we are not getting here and vice versa basically we are setting serializer and deserializer
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+      RedisTemplate redisTemplate = new RedisTemplate<>();
+      redisTemplate.setConnectionFactory(factory);
+      redisTemplate.setKeySerializer(new StringRedisSerializer());
+      redisTemplate.setValueSerializer(new StringRedisSerializer());
+     return redisTemplate;
+    }
+}
--- src/test/java/net/engineeringdigest/journalApp/service/RedisTest.java
+++ src/test/java/net/engineeringdigest/journalApp/service/RedisTest.java
@@ -5,17 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
-@SpringBootTest
+//@SpringBootTest
 public class RedisTest {
-
-    @Autowired
-    RedisTemplate redisTemplate;
-
-    @Test
-    void redisTest(){
-        redisTemplate.opsForValue().set("email","rajatrana2309@gmail.com");
-        Object email = redisTemplate.opsForValue().get("email");
-        int a= 1;
-    }
+//
+//    @Autowired
+//    RedisTemplate redisTemplate;
+//
+//    @Test
+//    void redisTest(){
+//        redisTemplate.opsForValue().set("email","rajatrana2309@gmail.com");
+//        Object email = redisTemplate.opsForValue().get("email");
+//        int a= 1;
+//    }
 
 }
