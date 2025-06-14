2e83d128959546d50d057a2a0a3f1122af28d86f
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-06-14 23:25:13 +0530

    re writing configuration class of reddis

--- src/main/java/net/engineeringdigest/journalApp/config/RedisConfg.java
+++ src/main/java/net/engineeringdigest/journalApp/config/RedisConfg.java
@@ -10,12 +10,28 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfg {
 
     // why this clas basically when we are setting some value in redis cli we are not getting here and vice versa basically we are setting serializer and deserializer
+//    @Bean
+//    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+//      RedisTemplate redisTemplate = new RedisTemplate<>();
+//      redisTemplate.setConnectionFactory(factory);
+//      redisTemplate.setKeySerializer(new StringRedisSerializer());
+//      redisTemplate.setValueSerializer(new StringRedisSerializer());
+//     return redisTemplate;
+//    }
+
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
-      RedisTemplate redisTemplate = new RedisTemplate<>();
-      redisTemplate.setConnectionFactory(factory);
-      redisTemplate.setKeySerializer(new StringRedisSerializer());
-      redisTemplate.setValueSerializer(new StringRedisSerializer());
-     return redisTemplate;
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        // Use String serializer for both keys and values
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+
+        redisTemplate.afterPropertiesSet(); // ensures everything is initialized properly
+        return redisTemplate;
     }
 }
